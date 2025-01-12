5c393ab39ded4d3b903450e2514e7e5098b158aa
muhammedSuhail <suhailkzp1@gmail.com>
2025-01-13 00:13:17 +0530

    new things added in to stack

--- Data_structure/stack.js
+++ Data_structure/stack.js
@@ -9,6 +9,12 @@ class stack {
     pop() {
         return this.stk.pop()
     }
+    peek() { // return the last added value in to array , it should be return without removing (don't use pop)
+        return this.stk[this.stk.length - 1]
+    }
+    isEmty() {
+        return this.stk.length == 0
+    }
 }
 
 
@@ -17,5 +23,7 @@ stk.push(10)
 stk.push(20)
 stk.push(30)
 stk.push(40)
+console.log(stk.peek())
+console.log(stk.isEmty())
 
 console.log(stk.stk);
