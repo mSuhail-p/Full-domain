903aa9997af07de6ba1f3eecbc05ee4cfb283b05
muhammedSuhail <suhailkzp1@gmail.com>
2025-01-13 09:48:53 +0530

    Queue implementation

---
+++ Data_structure/queue.js
@@ -0,0 +1,36 @@
+
+
+// Queue implementaion 
+
+//  queue is first in first out
+class Queue {
+    constructor() {
+        this.que = []
+    }
+    enque(value) {
+        this.que.push(value)
+    }
+    dequeue() {
+        if (this.que.length > 0) {
+            return this.que.shift()
+        }
+
+        return 'no value'
+    }
+    peek() {
+        return this.que[0]
+    }
+    rear() {
+        return this.que[this.que.length - 1]
+    }
+    size() {
+        return this.que.length
+    }
+
+}
+
+let Q = new Queue()
+Q.enque(20)
+Q.enque(10)
+Q.enque(30)
+console.log(Q.size())
